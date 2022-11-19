4c7f6c668fe39aef3eb5e4534e18171050bc1036
Ajay <ajays7256@gamil.com>
2022-11-19 08:34:20 +0000

    update dropdown

--- application/views/admin/farmers/update_farmers.php
+++ application/views/admin/farmers/update_farmers.php
@@ -175,3 +175,51 @@ if(!empty($this->session->flashdata('emessage'))){ ?>
 
 <script type="text/javascript" src="<?php echo base_url() ?>assets/slider/ajaxupload.3.5.js"></script>
 <link href="<? echo base_url() ?>assets/cowadmin/css/jqvmap.css" rel='stylesheet' type='text/css' />
+
+
+
+<script >
+$(document).ready(function(){
+  	$("#states").change(function(){
+      // alert('hii');
+		var vf=$(this).val();
+    // alert(vf);
+		if(vf==""){
+			return false;
+
+		}else{
+			$('#cities option').remove();
+			  var opton="<option value=''>----Please Select-----</option>";
+			$.ajax({
+				url:base_url+"dcadmin/farmers/getfarmers/"+vf,
+				data : '',
+				type: "get",
+				success : function(html){
+						if(html!="NA")
+						{
+							var s = jQuery.parseJSON(html);
+							$.each(s, function(i) {
+							opton +='<option value="'+s[i]['cities_id']+'">'+s[i]['city_name']+'</option>';
+							});
+							$('#cities').append(opton);
+							//$('#city').append("<option value=''>Please Select State</option>");
+
+                      //var json = $.parseJSON(html);
+                      //var ayy = json[0].name;
+                      //var ayys = json[0].pincode;
+						}
+						else
+						{
+							alert('No subcategory Found');
+							return false;
+						}
+
+					}
+
+				})
+		}
+
+
+	})
+  });
+</script>
