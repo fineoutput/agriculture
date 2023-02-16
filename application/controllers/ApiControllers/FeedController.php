f320d24f2f088663da30f38f75ff6c1538fcff2a
Nitesh Shah <Fineoutput2021>
2023-02-16 20:14:40 +0530

    updates

--- application/controllers/ApiControllers/FeedController.php
+++ application/controllers/ApiControllers/FeedController.php
@@ -80,7 +80,7 @@ class FeedController extends CI_Controller
                 $weight = $this->input->post('weight');
                 $farmer_data = $this->db->get_where('tbl_farmers', array('is_active' => 1, 'auth' => $authentication))->result();
                 if (!empty($farmer_data)) {
-                    $dry_matter_intake = 33/100*$milk_yield + 2/100*$weight;
+                    $dry_matter_intake = 33 / 100 * $milk_yield + 2 / 100 * $weight;
                     $feed = $feed_percentage / 100 * $dry_matter_intake;
                     $fodder = $dry_matter_intake - $feed;
                     $feed_qty = 100 / 90 * $feed;
@@ -96,17 +96,17 @@ class FeedController extends CI_Controller
                     $silage = 100 / 30 * $silage_dm;
                     $data = [];
                     $data = array(
-                        'dry_matter_intake' => round($dry_matter_intake,2),
-                        'feed' => round($feed,2),
-                        'fodder' => round($fodder,2),
-                        'feed_qty' => round($feed_qty,2),
-                        'green_fodder' => round($green_fodder,2),
-                        'maize' => round($maize,2),
-                        'barseem' => round($barseem,2),
-                        'dry_fodder' => round($dry_fodder,2),
-                        'hary' => round($hary,2),
-                        'silage_dm' => round($silage_dm,2),
-                        'silage' => round($silage,2),
+                        'dry_matter_intake' => round($dry_matter_intake, 2),
+                        'feed' => round($feed, 2),
+                        'fodder' => round($fodder, 2),
+                        'feed_qty' => round($feed_qty, 2),
+                        'green_fodder' => round($green_fodder, 2),
+                        'maize' => round($maize, 2),
+                        'barseem' => round($barseem, 2),
+                        'dry_fodder' => round($dry_fodder, 2),
+                        'hary' => round($hary, 2),
+                        'silage_dm' => round($silage_dm, 2),
+                        'silage' => round($silage, 2),
                     );
                     $res = array(
                         'message' => "Success!",
@@ -157,20 +157,118 @@ class FeedController extends CI_Controller
                 $farmer_data = $this->db->get_where('tbl_farmers', array('is_active' => 1, 'auth' => $authentication))->result();
                 if (!empty($farmer_data)) {
                     $data = [];
-                    $dry_fodder=0;
+                    $cp = 0;
+                    $ee = 0;
+                    $cf = 0;
+                    $tdn = 0;
+                    $me = 0;
+                    $ca = 0;
+                    $p = 0;
+                    $adf = 0;
+                    $ndf = 0;
+                    $nel = 0;
+                    $rudp = 0;
+                    $endf = 0;
+                    $prot = json_decode($ProteinData);
+                    foreach ($prot as $prt) {
+                        if (!empty($prt[15])) {
+                            $cp += $prt[3] ? $prt[3] * $prt[15] / 100 : 0;
+                            $ee += $prt[4] ? $prt[4] * $prt[15] / 100 : 0;
+                            $cf +=  $prt[5] ? $prt[5] * $prt[15] / 100 : 0;
+                            $tdn +=  $prt[6] ? $prt[6] * $prt[15] / 100 : 0;
+                            $me +=  $prt[7] ? $prt[7] * $prt[15] / 100 : 0;
+                            $ca +=  $prt[8] ? $prt[8] * $prt[15] / 100 : 0;
+                            $p +=  $prt[9] ? $prt[9] * $prt[15] / 100 : 0;
+                            $adf += $prt[10] ? $prt[10] * $prt[15] / 100 : 0;
+                            $ndf +=  $prt[11] ? $prt[11] * $prt[15] / 100 : 0;
+                            $nel +=  $prt[12] ? $prt[12] * $prt[15] / 100 : 0;
+                            $rudp +=  $prt[13] ? $prt[13] * $prt[15] / 100 : 0;
+                            $endf +=  $prt[14] ? $prt[14] * $prt[15] / 100 : 0;
+                        }
+                    }
+                    $Energy = json_decode($EnergyData);
+                    foreach ($Energy as $enr) {
+                        if (!empty($enr[15])) {
+                            $cp += $enr[3] ?$enr[3] * $enr[15] / 100:0;
+                            $ee += $enr[4] ?$enr[4] * $enr[15] / 100:0;
+                            $cf += $enr[5] ?$enr[5] * $enr[15] / 100:0;
+                            $tdn += $enr[6] ?$enr[6] * $enr[15] / 100:0;
+                            $me += $enr[7] ?$enr[7] * $enr[15] / 100:0;
+                            $ca += $enr[8] ?$enr[8] * $enr[15] / 100:0;
+                            $p += $enr[9] ?$enr[9] * $enr[15] / 100:0;
+                            $adf += $enr[10] ?$enr[10] * $enr[15] / 100:0;
+                            $ndf += $enr[11] ?$enr[11] * $enr[15] / 100:0;
+                            $nel += $enr[12] ?$enr[12] * $enr[15] / 100:0;
+                            $rudp += $enr[13] ?$enr[13] * $enr[15] / 100:0;
+                            $endf += $enr[14] ?$enr[14] * $enr[15] / 100:0;
+                        }
+                    }
+                    $Product = json_decode($ProductData);
+                    foreach ($Product as $pro) {
+                        if (!empty($pro[15])) {
+                            $cp += $pro[3] ?$pro[3] * $pro[15] / 100:0;
+                            $ee += $pro[4] ?$pro[4] * $enr[15] / 100:0;
+                            $cf += $pro[5] ?$pro[5] * $pro[15] / 100:0;
+                            $tdn += $pro[6] ?$pro[6] * $pro[15] / 100:0;
+                            $me += $pro[7] ?$pro[7] * $pro[15] / 100:0;
+                            $ca += $pro[8] ?$pro[8] * $pro[15] / 100:0;
+                            $p += $pro[9] ?$pro[9] * $pro[15] / 100:0;
+                            $adf += $pro[10] ?$pro[10] * $pro[15] / 100:0;
+                            $ndf += $pro[11] ?$pro[11] * $pro[15] / 100:0;
+                            $nel += $pro[12] ?$pro[12] * $pro[15] / 100:0;
+                            $rudp += $pro[13] ?$pro[13] * $pro[15] / 100:0;
+                            $endf += $pro[14] ?$pro[14] * $pro[15] / 100:0;
+                        }
+                    }
+                    $Medicine = json_decode($MedicineData);
+                    foreach ($Medicine as $med) {
+                        if (!empty($med[15])) {
+                            $cp += $med[3] ?$med[3] * $med[15] / 100:0;
+                            $ee += $med[4] ?$med[4] * $med[15] / 100:0;
+                            $cf += $med[5] ?$med[5] * $med[15] / 100:0;
+                            $tdn += $med[6] ?$med[6] * $med[15] / 100:0;
+                            $me += $med[7] ?$med[7] * $med[15] / 100:0;
+                            $ca += $med[8] ?$med[8] * $med[15] / 100:0;
+                            $p += $med[9] ?$med[9] * $med[15] / 100:0;
+                            $adf += $med[10] ?$med[10] * $med[15] / 100:0;
+                            $ndf += $med[11] ?$med[11] * $med[15] / 100:0;
+                            $nel += $med[12] ?$med[12] * $med[15] / 100:0;
+                            $rudp += $med[13] ?$med[13] * $med[15] / 100:0;
+                            $endf += $med[14] ?$med[14] * $med[15] / 100:0;
+                        }
+                    }
+                    $fresh =  array(
+                        'CP' => round($cp, 2),
+                        'FAT' => round($ee, 2),
+                        'FIBER' => round($cf, 2),
+                        'TDN' => round($tdn, 2),
+                        'ENERGY' => round($me, 2),
+                        'CA' => round($ca, 2),
+                        'P' => round($p, 2),
+                        'RUDP' => round($adf, 2),
+                        'ADF' => round($ndf, 2),
+                        'NDF' => round($nel, 2),
+                        'NEL' => round($rudp, 2),
+                        'ENDF' => round($endf, 2),
+                    );
+                    $dmb =  array(
+                        'CP' => $cp>0?round(($cp * 12/100 +$cp), 2):0,
+                        'FAT' => $ee>0?round(($ee * 12/100 +$ee), 2):0,
+                        'FIBER' => $cf>0?round(($cf * 12/100 +$cf), 2):0,
+                        'TDN' => $tdn>0?round(($tdn * 12/100 +$tdn), 2):0,
+                        'ENERGY' => $me>0?round(($me * 12/100 +$me), 2):0,
+                        'CA' => $ca>0?round(($ca * 12/100 +$ca), 2):0,
+                        'P' => $p>0?round(($p * 12/100 +$p), 2):0,
+                        'RUDP' => $adf>0?round(($adf * 12/100 +$adf), 2):0,
+                        'ADF' => $ndf>0?round(($ndf * 12/100 +$ndf), 2):0,
+                        'NDF' => $nel>0?round(($nel * 12/100 +$nel), 2):0,
+                        'NEL' => $rudp>0?round(($rudp * 12/100 +$rudp), 2):0,
+                        'ENDF' => $endf>0?round(($endf * 12/100 +$endf), 2):0,
+                    );
+                    $dry_fodder = 0;
                     $data = array(
-                        'CP' => round($dry_fodder,2),
-                        'FAT' => round($dry_fodder,2),
-                        'FIBER' => round($dry_fodder,2),
-                        'TDN' => round($dry_fodder,2),
-                        'ENERGY' => round($dry_fodder,2),
-                        'CA' =>round($dry_fodder,2),
-                        'P' => round($dry_fodder,2),
-                        'RUDP' => round($dry_fodder,2),
-                        'ADF' => round($dry_fodder,2),
-                        'NDF' => round($dry_fodder,2),
-                        'NEL' => round($dry_fodder,2),
-                        'ENDF' => round($dry_fodder,2),
+                        'fresh' => $fresh,
+                        'dmb' => $dmb,
                     );
                     $res = array(
                         'message' => "Success!",
