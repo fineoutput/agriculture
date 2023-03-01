7db962a539fad3f6c0b841e54beae00321b794e4
Nitesh Shah <Fineoutput2021>
2023-03-01 21:14:30 +0530

    checkout api updates

--- application/controllers/ApiControllers/FarmerController.php
+++ application/controllers/ApiControllers/FarmerController.php
@@ -150,7 +150,7 @@ class FarmerController extends CI_Controller
                             'description_punjabi' => $ProData->description_punjabi,
                             'image' => $image,
                             // 'mrp' => $ProData->mrp,
-                            'selling_price' => $ProData->selling_price*$cart->qty,
+                            'selling_price' => $ProData->selling_price * $cart->qty,
                             // 'suffix' => $ProData->suffix,
                             'stock' => $stock,
                             'vendor_id' => $ProData->added_by,
@@ -286,6 +286,127 @@ class FarmerController extends CI_Controller
         } else {
             $res = array(
                 'message' => 'Please Insert Data',
+                'status' => 201
+            );
+            echo json_encode($res);
+        }
+    }
+    //============================================= Checkout ============================================//
+    public function Checkout()
+    {
+        $headers = apache_request_headers();
+        $authentication = $headers['Authentication'];
+        $farmer_data = $this->db->get_where('tbl_farmers', array('is_active' => 1, 'auth' => $authentication))->result();
+        //----- Verify Auth --------
+        if (!empty($farmer_data)) {
+            $CartData = $this->db->get_where('tbl_cart', array('farmer_id' => $farmer_data[0]->id))->result();
+            $data = [];
+            $total = 0;
+            $is_admin = 0;
+            $vendor_id = 0;
+            date_default_timezone_set("Asia/Calcutta");
+            $cur_date = date("Y-m-d H:i:s");
+            if (!empty($CartData)) {
+                foreach ($CartData as $cart) {
+                    $is_admin = $cart->is_admin;
+                    if ($cart->is_admin == 1) {
+                        //---admin products ----
+                        $ProData = $this->db->get_where('tbl_products', array('is_active' => 1, 'id' => $cart->product_id))->result();
+                    } else {
+                        //---vendor products ----
+                        $ProData = $this->db->get_where('tbl_products', array('is_active' => 1, 'id' => $cart->product_id))->result();
+                    }
+                    $ProData = $ProData[0];
+                    $vendor_id =  $ProData->added_by;
+                    if (!empty($ProData)) {
+                        //----- Check Inventory  --------
+                        if ($ProData->inventory < $cart->qty) {
+                            $res = array(
+                                'message' => $ProData->name . ' is out of stock. Please remove this from cart!',
+                                'status' => 201
+                            );
+                            echo json_encode($res);
+                        }
+                        $total += $ProData->selling_price * $cart->qty;
+                    } else {
+                        $this->db->delete('tbl_cart', array('farmer_id' => $farmer_data[0]->id, 'product_id' => $cart->product_id));
+                    }
+                }
+                //------- order1 entry -----------
+                $Order1Data = array(
+                    'farmer_id' => $farmer_data[0]->id,
+                    'is_admin' => $is_admin,
+                    'vendor_id' => $vendor_id,
+                    'final_amount' => $total,
+                    'date' => $cur_date,
+                );
+                $order1_id = $this->base_model->insert_table("tbl_order1", $Order1Data, 1);
+                //------- order2 entry -----------
+                foreach ($CartData as $cart) {
+                    if ($cart->is_admin == 1) {
+                        //---admin products ----
+                        $ProData = $this->db->get_where('tbl_products', array('is_active' => 1, 'id' => $cart->product_id))->result();
+                    } else {
+                        //---vendor products ----
+                        $ProData = $this->db->get_where('tbl_products', array('is_active' => 1, 'id' => $cart->product_id))->result();
+                    }
+                    $ProData = $ProData[0];
+                    $Order2Data = array(
+                        'main_id' => $order1_id,
+                        'product_id' => $ProData->id,
+                        'qty' => $cart->qty,
+                        'mrp' => $ProData->mrp,
+                        'selling_price' => $ProData->selling_price,
+                        'gst' => $ProData->gst,
+                        'gst_price' => $ProData->gst_price,
+                        'selling_price_wo_gst' => $ProData->selling_price_wo_gst,
+                        'total_amount' => $ProData->selling_price * $cart->qty,
+                        'date' => $cur_date,
+                    );
+                    $order2_id = $this->base_model->insert_table("tbl_order2", $Order2Data, 1);
+                    $new_inventory = $ProData->inventory - $cart->qty;
+                    //--------- create inventory transaction -------
+                    $inv_txn = array(
+                        'order_id' => $order1_id,
+                        'at_time' => $ProData->inventory,
+                        'less_inventory' => $cart->qty,
+                        'updated_inventory' => $new_inventory,
+                        'date' => $cur_date,
+                    );
+                    $idd = $this->base_model->insert_table("tbl_inventory_txn", $inv_txn, 1);
+                    //------ Update inventory --------------------
+                    $data_update = array('inventory' => $new_inventory,);
+                    $this->db->where('id', $ProData->id);
+                    $zapak = $this->db->update('tbl_products', $data_update);
+                }
+                //--- Delete Cart -----------
+                $this->db->delete('tbl_cart', array('farmer_id' => $farmer_data[0]->id));
+                $count = $this->db->get_where('tbl_cart', array('farmer_id' => $farmer_data[0]->id))->num_rows();
+                $send = array(
+                    'count' => $count,
+                    'order_id' => $order1_id,
+                    'amount' => $total,
+                );
+                $res = array(
+                    'message' => "Success!",
+                    'status' => 200,
+                    'data' => $send,
+                );
+                echo json_encode($res);
+            } else {
+                $this->db->delete('tbl_cart', array('farmer_id' => $farmer_data[0]->id));
+                $count = $this->db->get_where('tbl_cart', array('farmer_id' => $farmer_data[0]->id))->num_rows();
+                $res = array(
+                    'message' => "Cart is empty!",
+                    'status' => 201,
+                    'data' => [],
+                    'count' => $count
+                );
+                echo json_encode($res);
+            }
+        } else {
+            $res = array(
+                'message' => 'Permission Denied!',
                 'status' => 201
             );
             echo json_encode($res);
