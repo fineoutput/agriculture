d3e90a09fee3f20ea0946a603e18dc88f5aaed7d
Nitesh Shah <Fineoutput2021>
2023-03-13 15:58:50 +0530

    order details and status update api

--- application/controllers/ApiControllers/VendorController.php
+++ application/controllers/ApiControllers/VendorController.php
@@ -11,7 +11,7 @@ class VendorController extends CI_Controller
         $this->load->model("admin/base_model");
         $this->load->library('pagination');
     }
-    //============================================= NewOrders ============================================//
+    //====================================== NewOrders =================================//
     public function NewOrders()
     {
         $headers = apache_request_headers();
@@ -59,7 +59,7 @@ class VendorController extends CI_Controller
             echo json_encode($res);
         }
     }
-    //============================================= AcceptedOrders ============================================//
+    //================================= AcceptedOrders ================================//
     public function AcceptedOrders()
     {
         $headers = apache_request_headers();
@@ -73,7 +73,6 @@ class VendorController extends CI_Controller
                 foreach ($OrderData as $order) {
                     $farData = $this->db->get_where('tbl_farmers', array('id' => $order->farmer_id))->result();
                     $pro_count = $this->db->get_where('tbl_order2', array('id' => $order->id))->num_rows();
-
                     $newDate = new DateTime($order->date);
                     $data[] = array(
                         'id' => $order->id,
@@ -108,7 +107,7 @@ class VendorController extends CI_Controller
             echo json_encode($res);
         }
     }
-    //============================================= AcceptedOrders ============================================//
+    //============================== DispatchedOrders =================================//
     public function DispatchedOrders()
     {
         $headers = apache_request_headers();
@@ -122,7 +121,6 @@ class VendorController extends CI_Controller
                 foreach ($OrderData as $order) {
                     $farData = $this->db->get_where('tbl_farmers', array('id' => $order->farmer_id))->result();
                     $pro_count = $this->db->get_where('tbl_order2', array('id' => $order->id))->num_rows();
-
                     $newDate = new DateTime($order->date);
                     $data[] = array(
                         'id' => $order->id,
@@ -157,8 +155,8 @@ class VendorController extends CI_Controller
             echo json_encode($res);
         }
     }
-    //============================================= completedOrders ============================================//
-    public function completedOrders()
+    //========================= CompletedOrders ====================================//
+    public function CompletedOrders()
     {
         $headers = apache_request_headers();
         $authentication = $headers['Authentication'];
@@ -171,7 +169,6 @@ class VendorController extends CI_Controller
                 foreach ($OrderData as $order) {
                     $farData = $this->db->get_where('tbl_farmers', array('id' => $order->farmer_id))->result();
                     $pro_count = $this->db->get_where('tbl_order2', array('id' => $order->id))->num_rows();
-
                     $newDate = new DateTime($order->date);
                     $data[] = array(
                         'id' => $order->id,
@@ -206,7 +203,7 @@ class VendorController extends CI_Controller
             echo json_encode($res);
         }
     }
-    //============================================= RejectedOrders ============================================//
+    //============================== RejectedOrders ===============================//
     public function RejectedOrders()
     {
         $headers = apache_request_headers();
@@ -220,7 +217,6 @@ class VendorController extends CI_Controller
                 foreach ($OrderData as $order) {
                     $farData = $this->db->get_where('tbl_farmers', array('id' => $order->farmer_id))->result();
                     $pro_count = $this->db->get_where('tbl_order2', array('id' => $order->id))->num_rows();
-
                     $newDate = new DateTime($order->date);
                     $data[] = array(
                         'id' => $order->id,
@@ -250,6 +246,162 @@ class VendorController extends CI_Controller
         } else {
             $res = array(
                 'message' => 'Permission Denied!',
+                'status' => 201
+            );
+            echo json_encode($res);
+        }
+    }
+    //============================== OrderDetails ===============================//
+    public function OrderDetails($id)
+    {
+        $headers = apache_request_headers();
+        $authentication = $headers['Authentication'];
+        $vendor_data = $this->db->get_where('tbl_vendor', array('is_active' => 1, 'is_approved' => 1, 'auth' => $authentication))->result();
+        //----- Verify Auth --------
+        if (!empty($vendor_data)) {
+            $OrderData = $this->db->order_by('id', 'desc')->get_where('tbl_order1', array('vendor_id' => $vendor_data[0]->id, 'is_admin' => 0, 'payment_status' => 1, 'id' => $id))->result();
+            $data = [];
+            if (!empty($OrderData)) {
+                $Order2Data = $this->db->get_where('tbl_order2', array('main_id' => $id))->result();
+                foreach ($Order2Data as $order) {
+                    if (!empty($order->image)) {
+                        $image = base_url() . $order->image;
+                    } else {
+                        $image = '';
+                    }
+                    $data[] = array(
+                        'pro_name' => $order->id,
+                        'pro_image' => $image,
+                        'qty' => $order->qty,
+                        'selling_price' => $order->selling_price,
+                        'total_amount' => $order->total_amount,
+                    );
+                }
+                $res = array(
+                    'message' => "Success!",
+                    'status' => 200,
+                    'data' => $data,
+                );
+                echo json_encode($res);
+            } else {
+                $res = array(
+                    'message' => "No Orders Found!",
+                    'status' => 201,
+                    'data' => [],
+                );
+                echo json_encode($res);
+            }
+        } else {
+            $res = array(
+                'message' => 'Permission Denied!',
+                'status' => 201
+            );
+            echo json_encode($res);
+        }
+    }
+    //========================== Update Order Status ===============================//
+    public function UpdateOrderStatus()
+    {
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
+        $this->load->helper('security');
+        if ($this->input->post()) {
+            $headers = apache_request_headers();
+            $authentication = $headers['Authentication'];
+            $this->form_validation->set_rules('id', 'id', 'required|xss_clean|trim');
+            $this->form_validation->set_rules('status', 'status', 'required|xss_clean|trim');
+            if ($this->form_validation->run() == true) {
+                $id = $this->input->post('id');
+                $status = $this->input->post('status');
+                date_default_timezone_set("Asia/Calcutta");
+                $cur_date = date("Y-m-d H:i:s");
+                $vendor_data = $this->db->get_where('tbl_vendor', array('is_active' => 1, 'is_approved' => 1, 'auth' => $authentication))->result();
+                //----- Verify Auth --------
+                if (!empty($vendor_data)) {
+                    $zapak = '';
+                    if ($status == "accept") {
+                        $data_update = array(
+                            'order_status' => 2
+                        );
+                        $this->db->where('vendor_id', $vendor_data[0]->id); //vendor orders
+                        $this->db->where('is_admin', 0); //vendor orders
+                        $this->db->where('id', $id);
+                        $zapak = $this->db->update('tbl_order1', $data_update);
+                    }
+                    if ($status == "dispatch") {
+                        $data_update = array(
+                            'order_status' => 3
+                        );
+                        $this->db->where('vendor_id', $vendor_data[0]->id); //vendor orders
+                        $this->db->where('is_admin', 0); //vendor orders
+                        $this->db->where('id', $id);
+                        $zapak = $this->db->update('tbl_order1', $data_update);
+                    }
+                    if ($status == "complete") {
+                        $data_update = array(
+                            'order_status' => 4
+                        );
+                        $this->db->where('vendor_id', $vendor_data[0]->id); //vendor orders
+                        $this->db->where('is_admin', 0); //vendor orders
+                        $this->db->where('id', $id);
+                        $zapak = $this->db->update('tbl_order1', $data_update);
+                    }
+                    if ($t == "reject") {
+                        $data_update = array('order_status' => 5);
+                        $this->db->where('vendor_id', $vendor_data[0]->id); //vendor orders
+                        $this->db->where('is_admin', 0); //vendor orders
+                        $this->db->where('id', $id);
+                        $zapak = $this->db->update('tbl_order1', $data_update);
+                        //-------update inventory-------
+                        $this->db->select('*');
+                        $this->db->from('tbl_order2');
+                        $this->db->where('main_id', $id);
+                        $data_order2 = $this->db->get();
+                        foreach ($data_order2->result() as $data) {
+                            $this->db->select('*');
+                            $this->db->from('tbl_products');
+                            $this->db->where('id', $data->product_id);
+                            $pro_data = $this->db->get()->row();
+                            if (!empty($pro_data)) {
+                                $update_inv = $pro_data->inventory + $data->quantity;
+                                $data_update = array('inventory' => $update_inv);
+                                $this->db->where('id', $pro_data->id);
+                                $this->db->where('added_by', $vendor_data[0]->id); //vendor orders
+                                $this->db->where('is_admin', 0); //vendor orders
+                                $zapak2 = $this->db->update('tbl_products', $data_update);
+                            }
+                        }
+                    }
+                    if (!empty($zapak)) {
+                        $res = array(
+                            'message' => "Status updated successfully!",
+                            'status' => 200,
+                        );
+                        echo json_encode($res);
+                    } else {
+                        $res = array(
+                            'message' => "Some error occurred!",
+                            'status' => 201,
+                        );
+                        echo json_encode($res);
+                    }
+                } else {
+                    $res = array(
+                        'message' => 'Permission Denied!',
+                        'status' => 201
+                    );
+                    echo json_encode($res);
+                }
+            } else {
+                $res = array(
+                    'message' => validation_errors(),
+                    'status' => 201
+                );
+                echo json_encode($res);
+            }
+        } else {
+            $res = array(
+                'message' => 'Please Insert Data',
                 'status' => 201
             );
             echo json_encode($res);
