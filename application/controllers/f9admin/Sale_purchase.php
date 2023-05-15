276c6f75c456bbb293e0cd66a3340f3d1a5349b0
Divyanshu <divyanshu6800@gmail.com>
2023-05-15 11:53:31 +0530

    farmer sale purchase view status wise

---
+++ application/controllers/f9admin/Sale_purchase.php
@@ -0,0 +1,205 @@
+<?php
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+require_once(APPPATH . 'core/CI_finecontrol.php');
+class Sale_purchase extends CI_finecontrol
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("login_model");
+        $this->load->model("admin/base_model");
+        $this->load->library('user_agent');
+    }
+
+    public function view_salepurchase()
+    {
+
+        if (!empty($this->session->userdata('admin_data'))) {
+
+
+            $data['user_name'] = $this->load->get_var('user_name');
+
+            $this->db->select('*');
+            $this->db->from('tbl_sale_purchase');
+            //$this->db->where('id',$usr);
+            $data['sale_purchase'] = $this->db->get();
+
+
+
+
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/sale_purchase/view_salepurchase');
+            $this->load->view('admin/common/footer_view');
+        } else {
+
+            redirect("login/admin_login", "refresh");
+        }
+    }
+
+    public function farmer_salepurchse_pending()
+    {
+        if (!empty($this->session->userdata('admin_data'))) {
+
+
+            $data['user_name'] = $this->load->get_var('user_name');
+
+            $this->db->select('*');
+            $this->db->from('tbl_sale_purchase');
+            $this->db->where('status', 0);
+            $data['sale_purchase'] = $this->db->get();
+            $data['heading'] ='Pending Sale/Purchase List';
+
+
+
+
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/sale_purchase/view_salepurchase');
+            $this->load->view('admin/common/footer_view');
+        } else {
+
+            redirect("login/admin_login", "refresh");
+        }
+    }
+    public function farmer_salepurchse_accepted()
+    {
+        if (!empty($this->session->userdata('admin_data'))) {
+
+
+            $data['user_name'] = $this->load->get_var('user_name');
+
+            $this->db->select('*');
+            $this->db->from('tbl_sale_purchase');
+            $this->db->where('status', 1);
+            $data['sale_purchase'] = $this->db->get();
+            $data['heading'] ='Accepted Sale/Purchase List';
+
+
+
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/sale_purchase/view_salepurchase');
+            $this->load->view('admin/common/footer_view');
+        } else {
+
+            redirect("login/admin_login", "refresh");
+        }
+    }
+    public function farmer_salepurchse_completed()
+    {
+        if (!empty($this->session->userdata('admin_data'))) {
+
+
+            $data['user_name'] = $this->load->get_var('user_name');
+
+            $this->db->select('*');
+            $this->db->from('tbl_sale_purchase');
+            $this->db->where('status', 2);
+            $data['sale_purchase'] = $this->db->get();
+            $data['heading'] ='Completed Sale/Purchase List';
+
+
+
+
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/sale_purchase/view_salepurchase');
+            $this->load->view('admin/common/footer_view');
+        } else {
+
+            redirect("login/admin_login", "refresh");
+        }
+    }
+    public function farmer_salepurchse_rejected()
+    {
+        if (!empty($this->session->userdata('admin_data'))) {
+
+
+            $data['user_name'] = $this->load->get_var('user_name');
+
+            $this->db->select('*');
+            $this->db->from('tbl_sale_purchase');
+            $this->db->where('status', 3);
+            $data['sale_purchase'] = $this->db->get();
+            $data['heading'] ='Rejected Sale/Purchase List';
+
+
+
+
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/sale_purchase/view_salepurchase');
+            $this->load->view('admin/common/footer_view');
+        } else {
+
+            redirect("login/admin_login", "refresh");
+        }
+    }
+
+    public function updatesalepurchaseStatus($idd, $t)
+    {
+
+        if (!empty($this->session->userdata('admin_data'))) {
+
+
+            $data['user_name'] = $this->load->get_var('user_name');
+
+
+            $id = base64_decode($idd);
+
+            if ($t == "accept") {
+
+                $data_update = array(
+                    'status' => 1
+
+                );
+
+                $this->db->where('id', $id);
+                $zapak = $this->db->update('tbl_sale_purchase', $data_update);
+
+                if ($zapak != 0) {
+                    redirect("dcadmin/sale_purchase/view_salepurchase", "refresh");
+                } else {
+                    echo "Error";
+                    exit;
+                }
+            }
+
+            if ($t == "reject") {
+
+                $data_update = array(
+                    'status' => 3
+
+                );
+
+                $this->db->where('id', $id);
+                $zapak = $this->db->update('tbl_sale_purchase', $data_update);
+
+                if ($zapak != 0) {
+                    redirect($_SERVER['HTTP_REFERER']);
+                } else {
+                    echo "Error";
+                    exit;
+                }
+            }
+            if ($t == "complete") {
+
+                $data_update = array(
+                    'status' => 2
+
+                );
+
+                $this->db->where('id', $id);
+                $zapak = $this->db->update('tbl_sale_purchase', $data_update);
+
+                if ($zapak != 0) {
+                    redirect($_SERVER['HTTP_REFERER']);
+                } else {
+                    echo "Error";
+                    exit;
+                }
+            }
+        } else {
+
+            $this->load->view('admin/login/index');
+        }
+    }
+}
---
+++ application/views/admin/sale_purchase/view_salepurchase.php
@@ -0,0 +1,158 @@
+ <div class="content-wrapper">
+     <section class="content-header">
+         <h1>
+             Farmer <?=$heading;  ?>
+         </h1>
+         <ol class="breadcrumb">
+             <li><a href="<?php echo base_url() ?>dcadmin/home"><i class="fa fa-dashboard"></i> Home</a></li>
+             <!-- <li><a href="<?php echo base_url() ?>admin/college"><i class="fa fa-dashboard"></i> All Team </a></li> -->
+             <li class="active">View Farmer   <?=$heading;  ?></li>
+         </ol>
+     </section>
+     <section class="content">
+         <div class="row">
+             <div class="col-lg-12">
+                 <!-- <a class="btn btn-info cticket" href="<?php echo base_url() ?>admin/home/add_team" role="button" style="margin-bottom:12px;"> Add Team</a> -->
+                 <div class="panel panel-default">
+                     <div class="panel-heading">
+                         <!-- <h3 class="panel-title"><i class="fa fa-money fa-fw"></i>View team</h3> -->
+                     </div>
+                     <div class="panel panel-default">
+
+                         <? if (!empty($this->session->flashdata('smessage'))) { ?>
+                             <div class="alert alert-success alert-dismissible">
+                                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                 <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                                 <? echo $this->session->flashdata('smessage'); ?>
+                             </div>
+                         <? }
+                            if (!empty($this->session->flashdata('emessage'))) { ?>
+                             <div class="alert alert-danger alert-dismissible">
+                                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                 <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+                                 <? echo $this->session->flashdata('emessage'); ?>
+                             </div>
+                         <? } ?>
+
+
+                         <div class="panel-body">
+                             <div class="box-body table-responsive no-padding">
+                                 <table class="table table-bordered table-hover table-striped" id="userTable">
+                                     <thead>
+                                         <tr>
+                                             <th>#</th>
+                                             <th>Information Type</th>
+                                             <th>Animal Name</th>
+                                             <th>Milk Production</th>
+                                             <th>Lactation</th>
+                                             <th>Location</th>
+                                             <th>Pastorate Pregnant</th>
+                                             <th>Expected Price</th>
+                                             <th>Image1</th>
+
+                                             <th>Action</th>
+                                         </tr>
+                                     </thead>
+                                     <tbody>
+                                         <?php $i = 1;
+                                            foreach ($sale_purchase->result() as $data) { ?>
+                                             <tr>
+                                                 <td><?php echo $i ?> </td>
+                                                 <td><?php echo $data->information_type ?></td>
+                                                 <td><?php echo $data->animal_name ?></td>
+                                                 <td><?php echo $data->milk_production ?></td>
+                                                 <td><?php echo $data->lactation ?></td>
+                                                 <td><?php echo $data->location ?></td>
+                                                 <td><?php echo $data->pastorate_pregnant ?></td>
+                                                 <td><?php echo "₹" . $data->expected_price ?></td>
+                                                 <td>
+                                                     <?php if ($data->image1 != "") {  ?>
+                                                         <img id="slide_img_path" height=50 width=100 src="<?php echo base_url() . $data->image1 ?>">
+                                                     <?php } else {  ?>
+                                                         Sorry No image Found
+                                                     <?php } ?>
+                                                 </td>
+                                                 <td>
+                                                     <div class="btn-group" id="btns<?php echo $i ?>">
+                                                         <div class="btn-group">
+                                                             <?php if ($data->status <= 1) { ?>
+                                                                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Action <span class="caret"></span></button>
+                                                                 <ul class="dropdown-menu" role="menu">
+
+                                                                     <?php if ($data->status == 0) { ?>
+                                                                         <li><a href="<?php echo base_url() ?>dcadmin/sale_purchase/updatesalepurchaseStatus/<?php echo base64_encode($data->id) ?>/accept">Accept</a></li>
+                                                                         <li><a href="<?php echo base_url() ?>dcadmin/sale_purchase/updatesalepurchaseStatus/<?php echo base64_encode($data->id) ?>/reject">Reject</a></li>
+                                                                     <?php } else if ($data->status == 1) {  ?>
+                                                                         <li><a href="<?php echo base_url() ?>dcadmin/sale_purchase/updatesalepurchaseStatus/<?php echo base64_encode($data->id) ?>/complete">Complete</a></li>
+                                                                     <?php }   ?>
+                                                                 </ul>
+                                                             <?php } else {
+                                                                    echo "NA";
+                                                                }  ?>
+                                                         </div>
+                                                     </div>
+
+                                                     <div style="display:none" id="cnfbox<?php echo $i ?>">
+                                                         <p> Are you sure delete this </p>
+                                                         <a href="<?php echo base_url() ?>admin/home/delete_team/<?php echo base64_encode($data->id); ?>" class="btn btn-danger">Yes</a>
+                                                         <a href="javasript:;" class="cans btn btn-default" mydatas="<?php echo $i ?>">No</a>
+                                                     </div>
+                                                 </td>
+                                             </tr>
+                                         <?php $i++;
+                                            } ?>
+                                     </tbody>
+                                 </table>
+
+
+
+
+
+
+                             </div>
+                         </div>
+                     </div>
+
+                 </div>
+
+             </div>
+         </div>
+     </section>
+ </div>
+
+
+ <style>
+     label {
+         margin: 5px;
+     }
+ </style>
+ <script src="<?php echo base_url() ?>assets/admin/plugins/datatables/jquery.dataTables.js"></script>
+ <script src="<?php echo base_url() ?>assets/admin/plugins/datatables/dataTables.bootstrap.js"></script>
+ <script type="text/javascript">
+     $$(document).ready(function() {
+         $$('#userTable').DataTable({
+             responsive: true,
+             // bSort: true
+         });
+
+         $$(document.body).on('click', '.dCnf', function() {
+             var i = $$(this).attr("mydata");
+             console.log(i);
+
+             $$("#btns" + i).hide();
+             $$("#cnfbox" + i).show();
+
+         });
+
+         $$(document.body).on('click', '.cans', function() {
+             var i = $$(this).attr("mydatas");
+             console.log(i);
+
+             $$("#btns" + i).show();
+             $$("#cnfbox" + i).hide();
+         })
+
+     });
+ </script>
+ <!-- <script type="text/javascript" src="<?php echo base_url() ?>assets/slider/ajaxupload.3.5.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets/slider/rs.js"></script>-->
