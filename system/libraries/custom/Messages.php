50303df4628d612cf5886bd863d7b54bd27db6e2
Nitesh Shah <Fineoutput2021>
2023-06-09 16:41:13 +0530

    updates

--- system/libraries/custom/Messages.php
+++ system/libraries/custom/Messages.php
@@ -34,4 +34,26 @@ class CI_Messages
     curl_close($curl);
     // echo $response;die();
     }
+    //=========================================== SENT MSG91 SMS =============================================
+    public function sendSmsMsg91($phone, $msg,$dlt)
+    {
+      $message = urlencode($msg);
+      $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'http://api.msg91.com/api/sendhttp.php?authkey='.SMSAUTH.'&mobile=91'.$phone.'&message='.$message.'&sender='.SMSID.'&DLT_TE_ID='.$dlt.'',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => array(
+        'Cookie: PHPSESSID=prqus0jgeu7bi43bp2d1hjgtv0'
+      ),
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    // echo $response;die();
+    }
 }
