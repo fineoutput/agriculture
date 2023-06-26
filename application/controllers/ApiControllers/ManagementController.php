1a0cfc9d83bf2d0c4092ea908c234bd6cdbc3599
Nitesh Shah <Fineoutput2021>
2023-06-26 15:46:01 +0530

    updates

--- application/controllers/ApiControllers/ManagementController.php
+++ application/controllers/ApiControllers/ManagementController.php
@@ -54,6 +54,25 @@ class ManagementController extends CI_Controller
         $farmer_data = $this->db->get_where('tbl_farmers', array('is_active' => 1, 'auth' => $authentication))->result();
         if (!empty($farmer_data)) {
           $entry_id = bin2hex(random_bytes(5));
+          //------ start check inventory ------
+          foreach ($data as $d) {
+            if (!empty($d->values->qty) && !empty($d->values->price) && !empty($d->values->amount)) {
+              if ($update_inventory == "Yes" && $d->values->type == 'feed' &&  $d->column != 'feed') {
+                $this->db->select_sum($d->column);
+                $this->db->where('farmer_id', $farmer_data[0]->id);
+                $query = $this->db->get();
+                if ($query->row()->$d->column >= $d->values->qty) {
+                  $res = array(
+                    'message' => $d->column+'available inventory is '+$query->row()->$d->column,
+                    'status' => 201
+                  );
+                  echo json_encode($res);
+                  exit();
+                }
+              }
+            }
+          }
+          //------ end check inventory ------
           foreach ($data as $d) {
             if (!empty($d->values->qty) && !empty($d->values->price) && !empty($d->values->amount)) {
               $data = array(
@@ -145,7 +164,7 @@ class ManagementController extends CI_Controller
         foreach ($daily_data as $daily) {
           $newdate = new DateTime($daily->date);
           $data[] = array(
-            'record_date' =>date('d-m-Y', strtotime($daily->record_date)),
+            'record_date' => date('d-m-Y', strtotime($daily->record_date)),
             'name' => $daily->name,
             'qty' => $daily->qty,
             'price' => $daily->price,
