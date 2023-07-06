c8ce7c23de4a25e647d156def870bc77a05fccce
Nitesh Shah <Fineoutput2021>
2023-07-06 13:43:30 +0530

    msg check

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -19,30 +19,97 @@ class Home extends CI_Controller
 	{
 		$this->load->view('privacy_policy');
 	}
-	// public function blog()
-	// {
-	//
-	//
-	// 											$this->db->select('*');
-	// 											$this->db->from('tbl_blog');
-	// 											$this->db->where('is_active',1);
-	// 											$this->db->order_by('blog_id', 'DESC');
-	// 											$data['blog_data']= $this->db->get();
-	//
-	//
-	//
-	//
-	// 		$this->load->view('blog/header',$data);
-	// 		$this->load->view('blog/blog');
-	// 		$this->load->view('blog/footer');
-	//     // }
-	// }
-	// public function single()
-	// {
-	//
-	// 		$this->load->view('blog/single-header');
-	// 		$this->load->view('blog/blogsingle');
-	// 		$this->load->view('blog/footer');
-	//
-	// }
+	public function check1()
+	{
+		//--- send welcome msg ---------
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://control.msg91.com/api/v5/flow/',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => '
+                                {
+                                "template_id": "649e7ef5d6fc055fd16f92a2",
+                                "sender": "514279",
+                                "short_url": "0",
+                                "mobiles": "918387039990"
+                                }
+                                ',
+			CURLOPT_HTTPHEADER => array(
+				'accept: application/json',
+				'authkey: 396335ADmdafsq6458f062P1',
+				'content-type: application/json',
+				'Cookie: PHPSESSID=7nsedpqloairsa36o6h6iivst5'
+			),
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+	}
+	public function check2()
+	{
+		//--- send welcome msg ---------
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://control.msg91.com/api/v5/flow/',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => '
+                                {
+                                "template_id": "649e7f1bd6fc0504df28fcc3",
+                                "sender": "514279",
+                                "short_url": "0",
+								"mobiles": "918387039990"
+                                }
+                                ',
+			CURLOPT_HTTPHEADER => array(
+				'accept: application/json',
+				'authkey: 396335ADmdafsq6458f062P1',
+				'content-type: application/json',
+				'Cookie: PHPSESSID=7nsedpqloairsa36o6h6iivst5'
+			),
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+	}
+	public function check3()
+	{
+		//--- send welcome msg ---------
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => 'https://control.msg91.com/api/v5/flow/',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => '
+                                {
+                                "template_id": "649e7f76d6fc056e32336712",
+                                "sender": "514279",
+                                "short_url": "0",
+                                "mobiles": "918387039990"
+                                }
+                                ',
+			CURLOPT_HTTPHEADER => array(
+				'accept: application/json',
+				'authkey: 396335ADmdafsq6458f062P1',
+				'content-type: application/json',
+				'Cookie: PHPSESSID=7nsedpqloairsa36o6h6iivst5'
+			),
+		));
+		$response = curl_exec($curl);
+		curl_close($curl);
+	}
 }
