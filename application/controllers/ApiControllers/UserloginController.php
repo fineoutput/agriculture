054f9f9bf6ab5ef62859ebc514a045b0f505f1fe
Nitesh Shah <niteshshah000@gmail.com>
2024-04-15 11:27:23 +0530

    dctor and vendor login  api

--- application/controllers/ApiControllers/UserloginController.php
+++ application/controllers/ApiControllers/UserloginController.php
@@ -22,7 +22,7 @@ class UserloginController extends CI_Controller
     }
     //======================================USER LOGIN API CONTROLLER START========================//
     //======================================= USER REGISTER PROCESS===================================//
-    public function register_process()
+    public function farmer_register_process()
     {
         $this->load->helper(array('form', 'url'));
         $this->load->library('form_validation');
@@ -130,7 +130,7 @@ class UserloginController extends CI_Controller
                     'expert_category' => $expert_category,
                 );
                 //-------------- register user  with otp ------------
-                $Register = $this->login->RegisterOtpVerify($send);
+                $Register = $this->login->farmer_RegisterOtpVerify($send);
                 echo $Register;
             } else {
                 $respone['status'] = false;
@@ -143,35 +143,8 @@ class UserloginController extends CI_Controller
             echo json_encode($respone);
         }
     }
-    //============================== USER REGISTER OTP VERIFY =====================================//
-    public function register_otp_verify()
-    {
-        $this->load->helper(array('form', 'url'));
-        $this->load->library('form_validation');
-        $this->load->helper('security');
-        if ($this->input->post()) {
-            $this->form_validation->set_rules('phone', 'phone', 'required|xss_clean|trim');
-            $this->form_validation->set_rules('otp', 'otp', 'required|xss_clean|trim');
-            if ($this->form_validation->run() == true) {
-                $phone = $this->input->post('phone');
-                $otp = $this->input->post('otp');
-                //-------------- register otp verify ------------
-                $RegisterVerify = $this->login->RegisterOtpVerify($phone, $otp);
-                // redirect($_SERVER['HTTP_REFERER']);
-                echo $RegisterVerify;
-            } else {
-                $respone['status'] = false;
-                $respone['message'] = validation_errors();
-                echo json_encode($respone);
-            }
-        } else {
-            $respone['status'] = false;
-            $respone['message'] = 'Please Insert Some Data';
-            echo json_encode($respone);
-        }
-    }
     //==================================== USER LOGIN PROCESS ========================================//
-    public function login_process()
+    public function farmer_login_process()
     {
         $this->load->helper(array('form', 'url'));
         $this->load->library('form_validation');
@@ -183,7 +156,7 @@ class UserloginController extends CI_Controller
                 $phone = $this->input->post('phone');
                // $type = $this->input->post('type');
                 //------ user login send otp ----------
-                $Login = $this->login->LoginWithOtp($phone);
+                $Login = $this->login->farmer_LoginWithOtp($phone);
                 echo $Login;
             } else {
                 $respone['status'] = false;
@@ -197,7 +170,7 @@ class UserloginController extends CI_Controller
         }
     }
     //================================== USER LOGIN OTP VERIFY =======================================//
-    public function login_otp_verify()
+    public function farmer_login_otp_verify()
     {
         $this->load->helper(array('form', 'url'));
         $this->load->library('form_validation');
@@ -211,7 +184,7 @@ class UserloginController extends CI_Controller
                 $otp = $this->input->post('otp');
                 $type = $this->input->post('type');
                 //-------------- register otp verify ------------
-                $LoginVerify = $this->login->LoginOtpVerify($phone, $otp ,$type);
+                $LoginVerify = $this->login->farmer_LoginOtpVerify($phone, $otp ,$type);
                 echo $LoginVerify;
             } else {
                 $respone['status'] = false;
@@ -224,6 +197,208 @@ class UserloginController extends CI_Controller
             echo json_encode($respone);
         }
     }
+
+      //======================================= doctor and vendor REGISTER PROCESS===================================//
+      public function register_process()
+      {
+          $this->load->helper(array('form', 'url'));
+          $this->load->library('form_validation');
+          $this->load->helper('security');
+          if ($this->input->post()) {
+              $this->form_validation->set_rules('name', 'name', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('village', 'village', 'xss_clean|trim');
+              $this->form_validation->set_rules('district', 'district', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('city', 'city', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('state', 'state', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('pincode', 'pincode', 'xss_clean|trim');
+              $this->form_validation->set_rules('phone', 'phone', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('type', 'type', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('email', 'email', 'xss_clean|trim');
+              $this->form_validation->set_rules('doc_type', 'doc_type', 'xss_clean|trim');
+              $this->form_validation->set_rules('degree', 'degree', 'xss_clean|trim');
+              $this->form_validation->set_rules('experience', 'experience', 'xss_clean|trim');
+              // $this->form_validation->set_rules('qualification', 'qualification', 'xss_clean|trim');
+              $this->form_validation->set_rules('shop_name', 'shop_name', 'xss_clean|trim');
+              $this->form_validation->set_rules('address', 'address', 'xss_clean|trim');
+              $this->form_validation->set_rules('gst_no', 'gst_no', 'xss_clean|trim');
+              $this->form_validation->set_rules('aadhar_no', 'aadhar_no', 'xss_clean|trim');
+              $this->form_validation->set_rules('pan_no', 'pan_no', 'xss_clean|trim');
+              $this->form_validation->set_rules('latitude', 'latitude', 'xss_clean|trim');
+              $this->form_validation->set_rules('longitude', 'longitude', 'xss_clean|trim');
+              $this->form_validation->set_rules('no_of_animals', 'no_of_animals', 'xss_clean|trim');
+              $this->form_validation->set_rules('expert_category', 'expert_category', 'xss_clean');
+  
+              if ($this->form_validation->run() == true) {
+                  $name = $this->input->post('name');
+                  $village = $this->input->post('village');
+                  $district = $this->input->post('district');
+                  $city = $this->input->post('city');
+                  $state = $this->input->post('state');
+                  $pincode = $this->input->post('pincode');
+                  $phone = $this->input->post('phone');
+                  $email = $this->input->post('email');
+                  $doc_type = $this->input->post('doc_type');
+                  $degree = $this->input->post('degree');
+                  $experience = $this->input->post('experience');
+                  // $qualification = $this->input->post('qualification');
+                  $shop_name = $this->input->post('shop_name');
+                  $address = $this->input->post('address');
+                  $gst_no = $this->input->post('gst_no');
+                  $aadhar_no = $this->input->post('aadhar_no');
+                  $pan_no = $this->input->post('pan_no');
+                  $type = $this->input->post('type');
+                  $latitude = $this->input->post('latitude');
+                  $longitude = $this->input->post('longitude');
+                  $no_animals = $this->input->post('no_of_animals');
+                  $expert_category = $this->input->post('expert_category');
+                  $this->load->library('upload');
+                  $image = '';
+                  $img1 = 'image';
+                  if (!empty($_FILES['image'])) {
+                      $file_check = ($_FILES['image']['error']);
+                      if ($file_check != 4) {
+                          $image_upload_folder = FCPATH . "assets/uploads/aadhar/";
+                          if (!file_exists($image_upload_folder)) {
+                              mkdir($image_upload_folder, DIR_WRITE_MODE, true);
+                          }
+                          $new_file_name = "image" . date("YmdHis");
+                          $this->upload_config = array(
+                              'upload_path'   => $image_upload_folder,
+                              'file_name' => $new_file_name,
+                              'allowed_types' => 'jpg|jpeg|png',
+                              'max_size'      => 25000
+                          );
+                          $this->upload->initialize($this->upload_config);
+                          if (!$this->upload->do_upload($img1)) {
+                              $upload_error = $this->upload->display_errors();
+                              $respone['status'] = false;
+                              $respone['message'] = $upload_error;
+                              echo json_encode($respone);
+                              die();
+                          } else {
+                              $file_info = $this->upload->data();
+                              $image = "assets/uploads/aadhar/" . $file_info['file_name'];
+                          }
+                      }
+                  }
+                  $send = array(
+                      'name' => $name,
+                      'village' => $village,
+                      'district' => $district,
+                      'city' => $city,
+                      'state' => $state,
+                      'pincode' => $pincode,
+                      'phone' => $phone,
+                      'email' => $email,
+                      'image' => $image,
+                      'doc_type' => $doc_type,
+                      'degree' => $degree,
+                      'experience' => $experience,
+                      // 'qualification' => $qualification,
+                      'shop_name' => $shop_name,
+                      'address' => $address,
+                      'gst_no' => $gst_no,
+                      'aadhar_no' => $aadhar_no,
+                      'pan_no' => $pan_no,
+                      'type' => $type,
+                      'latitude' => $latitude,
+                      'longitude' => $longitude,
+                      'no_animals' => $no_animals,
+                      'expert_category' => $expert_category,
+                  );
+                  //-------------- register user  with otp ------------
+                  $Register = $this->login->RegisterWithOtp($send);
+                  echo $Register;
+              } else {
+                  $respone['status'] = false;
+                  $respone['message'] = validation_errors();
+                  echo json_encode($respone);
+              }
+          } else {
+              $respone['status'] = false;
+              $respone['message'] = 'Please Insert Some Data';
+              echo json_encode($respone);
+          }
+      }
+      //============================== USER REGISTER OTP VERIFY =====================================//
+      public function register_otp_verify()
+      {
+          $this->load->helper(array('form', 'url'));
+          $this->load->library('form_validation');
+          $this->load->helper('security');
+          if ($this->input->post()) {
+              $this->form_validation->set_rules('phone', 'phone', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('otp', 'otp', 'required|xss_clean|trim');
+              if ($this->form_validation->run() == true) {
+                  $phone = $this->input->post('phone');
+                  $otp = $this->input->post('otp');
+                  //-------------- register otp verify ------------
+                  $RegisterVerify = $this->login->RegisterOtpVerify($phone, $otp);
+                  // redirect($_SERVER['HTTP_REFERER']);
+                  echo $RegisterVerify;
+              } else {
+                  $respone['status'] = false;
+                  $respone['message'] = validation_errors();
+                  echo json_encode($respone);
+              }
+          } else {
+              $respone['status'] = false;
+              $respone['message'] = 'Please Insert Some Data';
+              echo json_encode($respone);
+          }
+      }
+      //==================================== USER LOGIN PROCESS ========================================//
+      public function login_process()
+      {
+          $this->load->helper(array('form', 'url'));
+          $this->load->library('form_validation');
+          $this->load->helper('security');
+          if ($this->input->post()) {
+              $this->form_validation->set_rules('phone', 'phone', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('type', 'type', 'required|xss_clean|trim');
+              if ($this->form_validation->run() == true) {
+                  $phone = $this->input->post('phone');
+                  $type = $this->input->post('type');
+                  //------ user login send otp ----------
+                  $Login = $this->login->LoginWithOtp($phone, $type);
+                  echo $Login;
+              } else {
+                  $respone['status'] = false;
+                  $respone['message'] = validation_errors();
+                  echo json_encode($respone);
+              }
+          } else {
+              $respone['status'] = false;
+              $respone['message'] = 'Please Insert Some Data';
+              echo json_encode($respone);
+          }
+      }
+      //================================== USER LOGIN OTP VERIFY =======================================//
+      public function login_otp_verify()
+      {
+          $this->load->helper(array('form', 'url'));
+          $this->load->library('form_validation');
+          $this->load->helper('security');
+          if ($this->input->post()) {
+              $this->form_validation->set_rules('phone', 'phone', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('otp', 'otp', 'required|xss_clean|trim');
+              if ($this->form_validation->run() == true) {
+                  $phone = $this->input->post('phone');
+                  $otp = $this->input->post('otp');
+                  //-------------- register otp verify ------------
+                  $LoginVerify = $this->login->LoginOtpVerify($phone, $otp);
+                  echo $LoginVerify;
+              } else {
+                  $respone['status'] = false;
+                  $respone['message'] = validation_errors();
+                  echo json_encode($respone);
+              }
+          } else {
+              $respone['status'] = false;
+              $respone['message'] = 'Please Insert Some Data';
+              echo json_encode($respone);
+          }
+      }
     //================================ UPDATE USER PROFILE ===========================================//
     public function update_profile()
     {
