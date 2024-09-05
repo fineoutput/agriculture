06054b3a662fdce732d7469e0108005faeefbf84
Nitesh Shah <niteshshah000@gmail.com>
2024-09-05 20:41:31 +0530

    change in hash last letter mistake and change it l to I

--- system/libraries/custom/Login.php
+++ system/libraries/custom/Login.php
@@ -365,7 +365,7 @@ if (!empty($last_id2)) {
 //--------------- Send login OTP----- -----
 // $dlt = '645ca712d6fc053e3918af93';
 $dlt = '1407172223704961719';
-$message = "Dear User, your OTP for login on Dairy Muneem is $OTP and is valid for 5 minutes pUv%2BEzzZ5ml";
+$message = "Dear User, your OTP for login on Dairy Muneem is $OTP and is valid for 5 minutes pUv%2BEzzZ5mI";
 
 // $sendmsg = $this->CI->messages->sendOtpMsg91($phone, $OTP, $dlt);
 $sendmsg = $this->CI->messages->sendSmsMsg91($phone, $message, $dlt);
