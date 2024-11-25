36b912979366916df72c004910fe30f0d02e97ef
Nitesh Shah <niteshshah000@gmail.com>
2024-11-25 11:53:17 +0530

    validation solve

--- application/controllers/ApiControllers/UserloginController.php
+++ application/controllers/ApiControllers/UserloginController.php
@@ -30,9 +30,9 @@ class UserloginController extends CI_Controller
         if ($this->input->post()) {
             $this->form_validation->set_rules('name', 'name', 'required|xss_clean|trim');
             $this->form_validation->set_rules('village', 'village', 'xss_clean|trim');
-            $this->form_validation->set_rules('district', 'district', 'xss_clean|trim');
-            $this->form_validation->set_rules('city', 'city', 'xss_clean|trim');
-            $this->form_validation->set_rules('state', 'state', 'xss_clean|trim');
+            $this->form_validation->set_rules('district', 'district', 'required|xss_clean|trim');
+            $this->form_validation->set_rules('city', 'city', 'required|xss_clean|trim');
+            $this->form_validation->set_rules('state', 'state', 'required|xss_clean|trim');
             $this->form_validation->set_rules('pincode', 'pincode', 'xss_clean|trim');
             $this->form_validation->set_rules('phone', 'phone', 'required|xss_clean|trim');
             $this->form_validation->set_rules('type', 'type', 'required|xss_clean|trim');
@@ -204,7 +204,8 @@ class UserloginController extends CI_Controller
       public function register_process()
       {
 
-       
+       echo'hii';
+       exit;
           $this->load->helper(array('form', 'url'));
           $this->load->library('form_validation');
           $this->load->helper('security');
