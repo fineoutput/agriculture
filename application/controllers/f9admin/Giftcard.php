a8941ef4f6a278d6519f488818f04af22ffd843a
Nitesh Shah <niteshshah000@gmail.com>
2024-11-28 18:05:46 +0530

    gift card

---
+++ application/controllers/f9admin/Giftcard.php
@@ -0,0 +1,259 @@
+<?php
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+require_once(APPPATH . 'core/CI_finecontrol.php');
+class Giftcard extends CI_finecontrol
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("login_model");
+        $this->load->model("admin/base_model");
+        $this->load->library('user_agent');
+    }
+
+    public function index()
+    {
+    
+        if(!empty($this->session->userdata('admin_data'))){
+            $data['user_name']=$this->load->get_var('user_name');
+            $this->load->view('admin/common/header_view',$data);
+            $this->load->view('admin/giftcard/view_giftcard');
+            $this->load->view('admin/common/footer_view');
+            }
+            else{
+                redirect("login/admin_login","refresh");
+            }
+    
+   }
+
+   public function add_giftcard()
+    {
+        if (!empty($this->session->userdata('admin_data'))) {
+            $data['user_name'] = $this->load->get_var('user_name');
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/giftcard/add_giftcard');
+            $this->load->view('admin/common/footer_view');
+        } else {
+            redirect("login/admin_login", "refresh");
+        }
+    }
+
+
+    public function update_giftcard($idd)
+    {
+        if (!empty($this->session->userdata('admin_data'))) {
+
+            $id=base64_decode($idd);
+                     $data['id']=$idd;
+
+                $this->db->select('*');
+                            $this->db->from('gift_card');
+                            $this->db->where('id',$id);
+                            $dsa= $this->db->get();
+                            $data['gift']=$dsa->row();
+
+            $this->load->view('admin/common/header_view', $data);
+            $this->load->view('admin/giftcard/update_giftcard');
+            $this->load->view('admin/common/footer_view');
+        } else {
+            redirect("login/admin_login", "refresh");
+        }
+    }
+
+public function add_giftcard_data($t,$iw="")
+
+              {
+
+                if(!empty($this->session->userdata('admin_data'))){
+
+
+$this->load->helper(array('form', 'url'));
+            $this->load->library('form_validation');
+            $this->load->helper('security');
+            if($this->input->post())
+            {
+              // print_r($this->input->post());
+              // exit;
+              $this->form_validation->set_rules('amount', 'amount', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('price', 'price', 'required|xss_clean|trim');
+              $this->form_validation->set_rules('count', 'count', 'required|xss_clean|trim');
+        
+              if($this->form_validation->run()== TRUE)
+              {
+                $amount=$this->input->post('amount');
+                $price=$this->input->post('price');
+                $count=$this->input->post('count');
+
+                $ip = $this->input->ip_address();
+          date_default_timezone_set("Asia/Calcutta");
+                  $cur_date=date("Y-m-d H:i:s");
+
+                $addedby=$this->session->userdata('admin_id');
+
+          $typ=base64_decode($t);
+          if($typ==1){
+
+            $this->load->library('upload');
+                      $img1='image1';
+            
+                        $file_check=($_FILES['image1']['error']);
+                        if($file_check!=4){
+            $image_upload_folder = FCPATH . "assets/uploads/gift_card/";
+            if (!file_exists($image_upload_folder))
+            {
+            mkdir($image_upload_folder, DIR_WRITE_MODE, true);
+            }
+            $new_file_name="gift_card".date("Ymdhms");
+            $this->upload_config = array(
+            'upload_path'   => $image_upload_folder,
+            'file_name' => $new_file_name,
+            'allowed_types' =>'xlsx|csv|xls|pdf|doc|docx|txt|jpg|jpeg|png',
+            'max_size'      => 25000
+            );
+            $this->upload->initialize($this->upload_config);
+            if (!$this->upload->do_upload($img1))
+            {
+            $upload_error = $this->upload->display_errors();
+            // echo json_encode($upload_error);
+            echo $upload_error;
+            }
+            else
+            {
+            
+            $file_info = $this->upload->data();
+            
+            $videoNAmePath = "assets/uploads/gift_card/".$new_file_name.$file_info['file_ext'];
+            $file_info['new_name']=$videoNAmePath;
+            // $this->step6_model->updateappIconImage($imageNAmePath,$appInfoId);
+            $nnnn=$file_info['file_name'];
+            // echo json_encode($file_info);
+            }
+                        }
+
+          $data_insert = array('amount'=>$amount,
+                    'price'=>$price,
+                    'gift_count'=>$count,
+                    'image'=>$nnnn,
+                    'ip' =>$ip,
+                    'added_by' =>$addedby,
+                    'is_active' =>1,
+                    'date'=>$cur_date
+
+                    );
+
+
+
+
+
+          $last_id=$this->base_model->insert_table("gift_card",$data_insert,1) ;
+
+          }
+          if($typ==2){
+
+   $idw=base64_decode($iw);
+
+   $this->load->library('upload');
+                      $img1='image1';
+            
+                        $file_check=($_FILES['image1']['error']);
+                        if($file_check!=4){
+            $image_upload_folder = FCPATH . "assets/uploads/gift_card/";
+            if (!file_exists($image_upload_folder))
+            {
+            mkdir($image_upload_folder, DIR_WRITE_MODE, true);
+            }
+            $new_file_name="gift_card".date("Ymdhms");
+            $this->upload_config = array(
+            'upload_path'   => $image_upload_folder,
+            'file_name' => $new_file_name,
+            'allowed_types' =>'xlsx|csv|xls|pdf|doc|docx|txt|jpg|jpeg|png',
+            'max_size'      => 25000
+            );
+            $this->upload->initialize($this->upload_config);
+            if (!$this->upload->do_upload($img1))
+            {
+            $upload_error = $this->upload->display_errors();
+            // echo json_encode($upload_error);
+            echo $upload_error;
+            }
+            else
+            {
+            
+            $file_info = $this->upload->data();
+            
+            $videoNAmePath = "assets/uploads/gift_card/".$new_file_name.$file_info['file_ext'];
+            $file_info['new_name']=$videoNAmePath;
+            // $this->step6_model->updateappIconImage($imageNAmePath,$appInfoId);
+            $nnnn=$file_info['file_name'];
+            // echo json_encode($file_info);
+            }
+                        }
+
+      $data_insert = array('amount'=>$amount,
+                    'price'=>$price,
+                    'count'=>$count,
+                    'image'=>$nnnn,
+                    'is_active' =>1,
+                  
+
+                    );
+
+
+
+
+$this->db->where('id', $idw);
+            $last_id=$this->db->update('gift_card', $data_insert);
+
+          }
+
+
+                              if($last_id!=0){
+
+$this->session->set_flashdata('smessage','Data inserted successfully');
+
+                              redirect("dcadmin/Giftcard","refresh");
+
+                                      }
+
+                                      else
+
+                                      {
+
+$this->session->set_flashdata('emessage','Sorry error occured');
+redirect($_SERVER['HTTP_REFERER']);
+
+
+                                      }
+
+
+              }
+            else{
+
+$this->session->set_flashdata('emessage',validation_errors());
+     redirect($_SERVER['HTTP_REFERER']);
+
+            }
+
+            }
+          else{
+
+$this->session->set_flashdata('emessage','Please insert some data, No data available');
+     redirect($_SERVER['HTTP_REFERER']);
+
+          }
+          }
+          else{
+
+redirect("login/admin_login","refresh");
+
+
+          }
+
+          }
+
+       
+              
+    
+}
---
+++ application/views/admin/giftcard/add_giftcard.php
@@ -0,0 +1,87 @@
+<div class="content-wrapper">
+  <section class="content-header">
+    <h1>
+    Add New Gift Card
+    </h1>
+  </section>
+  <section class="content">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title"> Add New Gift Card</h3>
+          </div>
+          <? if (!empty($this->session->flashdata('smessage'))) {  ?>
+            <div class="alert alert-success alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+              <h4><i class="icon fa fa-check"></i> Alert!</h4>
+              <? echo $this->session->flashdata('smessage');  ?>
+            </div>
+          <? }
+          if (!empty($this->session->flashdata('emessage'))) {  ?>
+            <div class="alert alert-danger alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+              <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+              <? echo $this->session->flashdata('emessage');  ?>
+            </div>
+          <? }  ?>
+          <div class="panel-body">
+            <div class="col-lg-10">
+              <form action=" <?php echo base_url()  ?>dcadmin/Giftcard/add_giftcard_data/<? echo base64_encode(1) ?>" method="POST" id="slide_frm" enctype="multipart/form-data">
+                <div class="table-responsive">
+                  <table class="table table-hover">
+           
+                    <tr>
+                      <td> <strong>Amount</strong> <span style="color:red;">*</span></strong> </td>
+                      <td> <input type="text" name="amount"  class="form-control" placeholder="" required></td>
+                    </tr> 
+                    <tr>
+                      <td> <strong>Price</strong> <span style="color:red;">*</span></strong> </td>
+                      <td> <input type="text" name="price"  class="form-control" placeholder="" required></td>
+                    </tr> 
+                    <tr>
+                      <td> <strong>Gift Count</strong> <span style="color:red;">*</span></strong> </td>
+                      <td> <input type="text" name="count"  class="form-control" placeholder="" required></td>
+                    </tr>   
+                    <tr>
+    <td> <strong>Active</strong> <span style="color:red;">*</span> </td>
+    <td>
+        <label>
+            <input type="radio" name="is_active" value="1" class="form-control" required>
+            Yes
+        </label>
+        <label>
+            <input type="radio" name="is_active" value="0" class="form-control" required>
+            No
+        </label>
+    </td>
+</tr>
+<tr>
+                      <td> <strong>Image</strong> <span style="color:red;">*</span></strong> </td>
+                      <td> <input type="file" name="image1" class="form-control" placeholder="" required value="" /> </td>
+                    </tr>
+    
+                    <tr>
+                      <td colspan="2">
+                        <input type="submit" class="btn btn-success" value="save">
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+<script type="text/javascript" src=" <?php echo base_url()  ?>assets/slider/ajaxupload.3.5.js"></script>
+<link href=" <? echo base_url()  ?>assets/cowadmin/css/jqvmap.css" rel='stylesheet' type='text/css' />
+<script src="<?php echo base_url() ?>assets/admin/plugins/ckeditor/ckeditor.js"></script>
+<script>
+  // Replace the <textarea id="editor1"> with a CKEditor
+  // instance, using default configuration.
+  CKEDITOR.replace('editor1');
+  // CKEDITOR.instances['editor1'].setData('<p>nitesh</p><br><br><p>shah</p>');
+</script>
