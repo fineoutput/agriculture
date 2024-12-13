16051e0fcf798a9cf63092884203e89eff16cd9a
Nitesh Shah <niteshshah000@gmail.com>
2024-12-13 12:13:56 +0530

    change

--- application/controllers/ApiControllers/FeedController.php
+++ application/controllers/ApiControllers/FeedController.php
@@ -675,8 +675,7 @@ class FeedController extends CI_Controller
                     $objPHPExcel1->setActiveSheetIndex(3)->setCellValue('D12', $milk_return);
                     $i = 4;
                     $p = 7;
-                    print_r($material);
-                    exit;
+                    $material = json_decode($material);
                     if (is_array($material) || is_object($material)) {
                         foreach ($material as $mat) {
                             if ($mat->value == true) {
